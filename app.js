0f0801bc37ec3031badb2d4bdbdfe1d7d86f3348
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-03-01 14:40:30 -0500

    Attempt at random choice

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
 
-rockBtn.addEventListener("click", chooseRock);
+rockBtn.addEventListener("click", chooseRock, endGame);
+paperBtn.addEventListener("click", choosePaper);
+scissorsBtn.addEventListener("click", chooseScissors);
 
-function chooseRock () {
-    alert ("Rock");
+const gameArray = ['Rock', 'Paper', 'Scissors'];
+
+
+function chooseRock (gameArray) {
+    let compChoice = Math.floor(Math.random()*gameArray.length);
+    return compChoice; 
 }
+
+
+function choosePaper () {
+    alert ("Paper");
+}
+
+function chooseScissors () {
+    alert ("Scissors");
+}
+
+function endGame(compChoice) {
+    console.log(compChoice);
+}
+
+
+
+
+
+// function chooseRock (min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);      
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
