2912f6fb0ac00e16cf1c820929aa11d1ec955226
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-03-02 09:53:28 -0500

    Forgot to save Changes

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 
 function compChoice(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);      
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // 1 is rock
@@ -15,39 +15,37 @@ function compChoice(min, max) {
 // const gameArray = [0, 1, 2];
 // let compChoice;
 
-
-function chooseRock () {
-    compValue = compChoice(1, 3)
-    if (compValue === 1) {
-        alert('Computer Chose Rock. You tied!')
-    } else if (compValue === 2) {
-        alert('Computer Chose Sissors. You win!')
-    } else if (compValue === 3) {
-        alert('Computer Chose Paper. You lose!')
-    }
+function chooseRock() {
+  compValue = compChoice(1, 3);
+  if (compValue === 1) {
+    alert("Computer Chose Rock. You tied!");
+  } else if (compValue === 2) {
+    alert("Computer Chose Sissors. You win!");
+  } else if (compValue === 3) {
+    alert("Computer Chose Paper. You lose!");
+  }
 }
 
-
-function choosePaper () {
-    compValue = compChoice(1, 3)
-    if (compValue === 1) {
-        alert('Computer Chose Rock. You win!')
-    } else if (compValue === 2) {
-        alert('Computer Chose Sissors. You lose!')
-    } else if (compValue === 3) {
-        alert('Computer Chose Paper. You tied!')
-    }
+function choosePaper() {
+  compValue = compChoice(1, 3);
+  if (compValue === 1) {
+    alert("Computer Chose Rock. You win!");
+  } else if (compValue === 2) {
+    alert("Computer Chose Sissors. You lose!");
+  } else if (compValue === 3) {
+    alert("Computer Chose Paper. You tied!");
+  }
 }
 
-function chooseScissors () {
-    compValue = compChoice(1, 3)
-    if (compValue === 1) {
-        alert('Computer Chose Rock. You lose!')
-    } else if (compValue === 2) {
-        alert('Computer Chose Sissors. You tied!')
-    } else if (compValue === 3) {
-        alert('Computer Chose Paper. You win!')
-    }
+function chooseScissors() {
+  compValue = compChoice(1, 3);
+  if (compValue === 1) {
+    alert("Computer Chose Rock. You lose!");
+  } else if (compValue === 2) {
+    alert("Computer Chose Sissors. You tied!");
+  } else if (compValue === 3) {
+    alert("Computer Chose Paper. You win!");
+  }
 }
 
 // function endGame(compChoice) {
@@ -57,8 +55,6 @@ function chooseScissors () {
 // This is the random choice generator. Use this later!
 // let compChoice = Math.floor(Math.random()*gameArray.length);
 // return compChoice;
-
-
 
 rockBtn.addEventListener("click", chooseRock);
 paperBtn.addEventListener("click", choosePaper);
